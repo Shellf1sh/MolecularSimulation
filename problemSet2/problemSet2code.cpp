b6cbe99f1002782852f422e8a202c10029cedfd3
Markus <Markus.markusnp@gmail.com>
2023-04-28 14:16:33 +0200

    A more concise way to do the same thing. Now using arrays. Also added energy and angualr momentum variables

--- problemSet2/problemSet2code.cpp
+++ problemSet2/problemSet2code.cpp
@@ -2,39 +2,46 @@
 #include<cmath>
 
 class Dumbbell{
-    public: double m, k, delta_t; //When creating an instance of this class remember to define these variables
-    
-    public: double x1, y1, z1, x2, y2, z2; //Initialising the 6 position coordinates
-    public: double px1, py1, pz1, px2, py2, pz2; //Initialising the 6 position coordinates
-    public: double fx1, fy1, fz1, fx2, fy2, fz2; //Initialising the 6 force coordinates
+    public: 
+        double m, k, delta_t, energy; //When creating an instance of this class remember to define these variables
+        double angular_momentum[3];
+        
+        double r[6] = {0, 0, 0, 0, 0, 0}; //The elements are [x1, y1, z1, x2, y2, z2]
+        double p[6] = {0, 0, 0, 0, 0, 0}; //The elements are [px1, py1, pz1, px2, py2, pz2]
+        double F[6] = {0, 0, 0, 0, 0, 0}; //The elements are [fx1, fy1, fz1, fx2, fy2, fz2]
 
+        void updatePosition(){
+            for(int i = 0; i < 6; i++){
+                r[i] = r[i] + p[i]*delta_t/m;
+            }
+        }
 
-    public: void updatePosition(){
-        x1 = x1 + px1*delta_t/m;
-        y1 = y1 + py1*delta_t/m;
-        z1 = z1 + pz1*delta_t/m;
-        x2 = x2 + px2*delta_t/m;
-        y2 = y2 + py2*delta_t/m;
-        z2 = z2 + pz2*delta_t/m;
-    }
+        void updateMomentum(){
+            for(int i = 0; i < 6; i++){
+                p[i] = p[i] + F[i]*delta_t/2;
+            }
+        }
 
-    public: void updateMomentum(){
-        px1 = px1 + fx1*delta_t/2;
-        py1 = py1 + fy1*delta_t/2;
-        pz1 = pz1 + fz1*delta_t/2;
-        px2 = px2 + fx2*delta_t/2;
-        py2 = py2 + fy2*delta_t/2;
-        pz2 = pz2 + fz2*delta_t/2;
-    }
+        void calculateForce(){
+            for(int i = 0; i < 3; i++){
+                F[i] = -k*(r[i] - r[i+3]);
+                F[i+3] = k*(r[i] - r[i+3]);
+            }
+        }
 
-    public: void calculateForce(){
-        fx1 = -k*(x1-x2);
-        fy1 = -k*(y1-y2);
-        fz1 = -k*(z1-z2);
-        fx2 = k*(x1-x2);
-        fy2 = k*(y1-y2);
-        fz2 = k*(z1-z2);
-    }
+        void updateEnergy(){
+            double kinetic_energy = (p[0]*p[0]+p[1]*p[1]+p[2]*p[2]+p[3]*p[3]+p[4]*p[4]+p[5]*p[5])/m;
+
+            double potnetial_energy = 0.5*k*((r[0]-r[3])*(r[0]-r[3]) + (r[1]-r[4])*(r[1]-r[4]) + (r[2]-r[5])*(r[2]-r[5]));
+
+            energy = kinetic_energy + potnetial_energy;
+        }
+
+        void updateAngularMomentum(){
+            angular_momentum[0] = r[1]*p[2] - r[2]*p[1]; //TODO: Check that this is correct
+            angular_momentum[1] = r[2]*p[0] - r[0]*p[2];
+            angular_momentum[2] = r[0]*p[1] - r[1]*p[0];
+        }
 
 };
 
@@ -47,45 +54,37 @@ int main(){
     dumbbell.k = 0.4;
     dumbbell.delta_t = 1e-2;
 
-    dumbbell.x1 = 0.5; //Initial conditions: position 
-    dumbbell.y1 = 0;
-    dumbbell.z1 = 0;
-    dumbbell.x2 = -0.5;
-    dumbbell.y2 = 0;
-    dumbbell.z2 = 0;
-
-    dumbbell.px1 = 0;
-    dumbbell.py1 = 0.5*sqrt(dumbbell.m*dumbbell.k);
-    dumbbell.pz1 = 0;
-    dumbbell.px2 = 0;
-    dumbbell.py2 = -0.5*sqrt(dumbbell.m*dumbbell.k);
-    dumbbell.pz2 = 0;
+    dumbbell.r[0] = 0.5; // Initial conditions
+    dumbbell.r[3] = -0.5;
+    dumbbell.p[0] = 0.5*sqrt(dumbbell.m*dumbbell.k);
+    dumbbell.p[3] = -0.5*sqrt(dumbbell.m*dumbbell.k);
 
 
     int T = 10;
     int N = T/dumbbell.delta_t;
 
-    double trajectories[6][N];
+    double trajectories[N][6];
+
+    dumbbell.calculateForce();
 
     for(int i = 0; i < N; i++){
         dumbbell.updateMomentum(); //First step of the Verlet-velocity algorithm
         dumbbell.updatePosition(); //Second step
         dumbbell.calculateForce(); //Third step
         dumbbell.updateMomentum(); //Fourth step
+        
+        for(int j = 0; j < 6; j++){
+            trajectories[i][j] = dumbbell.r[j];
+        }
 
-        trajectories[0][i] = dumbbell.x1;
-        trajectories[1][i] = dumbbell.y1;
-        trajectories[2][i] = dumbbell.z1;
-        trajectories[3][i] = dumbbell.x2;
-        trajectories[4][i] = dumbbell.y2;
-        trajectories[5][i] = dumbbell.z2;
 
         if(i%100 == 0){
-            std::cout << dumbbell.x1 << "\n"; 
+            std::cout << dumbbell.r[0] << "\n"; 
         }
 
     }
 
+    //TODO save the trajectories array as a .csv file for plotting in python
 
     std::cout << "Done" << std::endl;
     return 0;
